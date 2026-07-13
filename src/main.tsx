555d4c0207fcdfcd9c891a3221b854dab983bb0a
Pantonyeung <107616897+Pantonyeung@users.noreply.github.com>
2026-07-14 01:05:57 +0800

    refactor: remove DOM patch and load unified SMT app

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import DineInItemSplitPatch from './DineInItemSplitPatch';
 import './styles.css';
-import './ui-v05.css';
-import './nav-fix.css';
-import './interaction-v061.css';
-import './dinein-item-split.css';
 
 const rootElement = document.getElementById('root');
-
-if (!rootElement) {
-  throw new Error('找不到 SMT 根節點');
-}
+if (!rootElement) throw new Error('找不到 SMT 根節點');
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
-    <DineInItemSplitPatch />
   </React.StrictMode>,
 );
 
