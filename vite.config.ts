72db7c34c860769734052894a78c0f05a9b566a8
Pantonyeung <107616897+Pantonyeung@users.noreply.github.com>
2026-07-14 09:31:01 +0800

    Emit legacy fallback and relative assets for T2s

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
+  plugins: [
+    react(),
+    legacy({
+      targets: ['Chrome >= 49', 'Android >= 7'],
+      renderLegacyChunks: true,
+    }),
+  ],
+  base: './',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    target: 'es2015',
   },
 });
