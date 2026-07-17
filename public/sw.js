9e0f803206a8c8d2136d20c85258a500e813e39f
Pantonyeung <107616897+Pantonyeung@users.noreply.github.com>
2026-07-17 23:07:46 +0800

    chore(pwa): refresh cache for Traditional Chinese SMT interface

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'morefun-smt-v2-statusbar-20260717-2';
+const CACHE_NAME = 'morefun-smt-v2-zh-hant-20260717-3';
 const APP_SHELL = ['./', './manifest.webmanifest', './smt-icon.svg'];
 
 self.addEventListener('install', (event) => {
